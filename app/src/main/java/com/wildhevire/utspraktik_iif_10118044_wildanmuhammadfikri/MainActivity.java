a9fa2ee28c243aa8487bb1c9e58142024a05de42
wildhevire <wildhevire404@gmail.com>
2021-06-05 10:18:15 +0700

    Init

---
+++ app/src/main/java/com/wildhevire/utspraktik_iif_10118044_wildanmuhammadfikri/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wildhevire.utspraktik_iif_10118044_wildanmuhammadfikri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
